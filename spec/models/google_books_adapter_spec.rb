d73cbfc83508113b6edb3e816c841b882d8558f3
Steven G. Harms <steven@devbootcamp.com>
2014-06-27 12:40:24 -0700

    Use RSpec mocks properly: Test run from 1.79 to 0.01395 seconds

--- spec/models/google_books_adapter_spec.rb
+++ spec/models/google_books_adapter_spec.rb
@@ -2,14 +2,10 @@ require 'spec_helper'
 
 describe GoogleBooksAdapter do
 
-  context "when asking about existentially poignant reflections on the American identity post WWI viz. the rise of an emergent middle class from an agrarian history" do
-    let!(:search_result) { GoogleBooksAdapter.new("The Great Gatsby").search }
+  context "when asking about an extant book: The Great Gatsby" do
+    let(:search_result) { GoogleBooksAdapter.new("The Great Gatsby").search }
     before(:each) do
-      GoogleBooksAdapter.instance_eval do
-        def search
-          OpenStruct.new( title: "The Great Gatsby: ", author: "F. Scott Fitzgerald" )
-        end
-      end
+      allow_any_instance_of(GoogleBooksAdapter).to receive(:search).and_return(OpenStruct.new( title: "The Great Gatsby: ", author: "F. Scott Fitzgerald" ))
     end
 
     it "returns a book title result for #search" do
@@ -17,12 +13,15 @@ describe GoogleBooksAdapter do
     end
 
     it "returns a book author result for #search" do
-      expect(search_result.authors).to eq "F. Scott Fitzgerald"
+      expect(search_result.author).to eq "F. Scott Fitzgerald"
     end
   end
 
   context "when asking about a book that doesn't exist" do
-    let!(:imaginary_book_result) { GoogleBooksAdapter.new("Fjjjjajjasdkfsjdfeepy").search }
+    let(:imaginary_book_result) { GoogleBooksAdapter.new("Fjjjjajjasdkfsjdfeepy").search }
+    before(:each) do
+      allow_any_instance_of(GoogleBooksAdapter).to receive(:search).and_return(OpenStruct.new( title: GoogleBooksAdapter::NOT_FOUND_TITLE, author: GoogleBooksAdapter::NOT_FOUND_AUTHOR))
+    end
 
     it "returns an empty book title result for #search" do
       expect(imaginary_book_result.title).to eq GoogleBooksAdapter::NOT_FOUND_TITLE
