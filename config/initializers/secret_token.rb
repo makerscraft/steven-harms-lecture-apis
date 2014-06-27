b559787a00c84f6e0020865808e0adbeae8038ae
Steven G. Harms <steven@devbootcamp.com>
2014-06-27 07:15:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiDemo::Application.config.secret_token = 'f37d1f2cf3984e6963979a62404693ee1283502439da5c3151e8437eb489c431a185857437b60d2ca8a4a282fb536d58b6d30e373b21abbf431c41979500980a'
