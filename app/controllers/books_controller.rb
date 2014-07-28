7d9d14b6de53c1e4a7e01448b4bc9001ad8d9e0e
Steven G. Harms <steven@devbootcamp.com>
2014-07-28 10:41:26 -0700

    Add BooksController and routes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,12 @@
+class BooksController < ApplicationController
+  def take_a_look
+  end
+
+  def enter_a_book
+    @title = "stub title"
+    @author = "stub author"
+  end
+
+  def reading_rainbow
+  end
+end
