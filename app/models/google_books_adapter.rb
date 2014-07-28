c1b9167b5b58bac54c88d2a1bd43ba559d128168
Steven G. Harms <steven@devbootcamp.com>
2014-07-28 11:10:23 -0700

    TDD: Green: Pass tests for GoogleBooks happy path

--- app/models/google_books_adapter.rb
+++ app/models/google_books_adapter.rb
@@ -1,2 +1,13 @@
 class GoogleBooksAdapter
+  def initialize(raw_response)
+    @raw_response = raw_response
+  end
+
+  def title
+    @raw_response[:title]
+  end
+
+  def author
+    @raw_response[:authors].first
+  end
 end
--- spec/models/google_books_adapter_spec.rb
+++ spec/models/google_books_adapter_spec.rb
@@ -4,7 +4,12 @@ describe GoogleBooksAdapter do
   describe "when searching for a book that exists" do
     let!(:googlebooks_response){ {title: "Either / Or", authors: [ "Soren Kierkergaard" ]} }
 
-    it "finds the author"
-    it "finds the title"
+    it "finds the author" do
+      expect(GoogleBooksAdapter.new(googlebooks_response).author).to eql "Soren Kierkergaard"
+    end
+
+    it "finds the title" do
+      expect(GoogleBooksAdapter.new(googlebooks_response).title).to eql "Either / Or"
+    end
   end
 end
